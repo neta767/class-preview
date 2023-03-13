04a790e05260305388bdfab39d13bacaae14f92e
neta767 <netakli@gmail.com>
2023-03-13 12:46:17 +0200

    t

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
-
+import Demo from "./Demo";
 function App() {
-  return <div className="App"></div>;
+  return <Demo />;
 }
 
 export default App;
---
+++ src/react-app-env.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react-scripts" />
+declare module "*.jfif";
+declare module "*.png";
+
